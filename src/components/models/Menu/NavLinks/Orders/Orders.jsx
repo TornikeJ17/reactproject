58d2c4b814a7c22d7444f607b66688877ee58088
TornikeJ17 <jmukhadze.tornike@hotmail.com>
2023-09-19 02:11:28 +0400

    changed

--- src/components/models/Menu/NavLinks/Orders/Orders.jsx
+++ src/components/models/Menu/NavLinks/Orders/Orders.jsx
@@ -1,22 +1,25 @@
 import React from "react";
-import cssStyles from './Orders.module.scss'
+import cssStyles from "./Orders.module.scss";
 import Cards from "../../../Widgets/Cards/Cards";
-
+import { orderIcons } from "../../../Icons/Icons";
 
 const Orders = () => {
-    
     return (
         <div className={cssStyles.ContainerOrders}>
             <h1>Orders</h1>
             <div className={cssStyles.Blocks}>
-                <Cards 
+                <Cards
                     width={"100%"}
                     height={"150px"}
                     element={
-                        <div>
-                            <div className={cssStyles.OrderIcons}></div>
-                            <div>Orders Completed</div>
-                            <div>4.5</div>
+                        <div className={cssStyles.OrderElementBlock}>
+                            <div>{orderIcons[0].icon}</div>
+                            <div className={cssStyles.OrderElementTitle}>
+                                Orders Completed
+                            </div>
+                            <div className={cssStyles.OrderElementText}>
+                                2345
+                            </div>
                         </div>
                     }
                 />
@@ -24,10 +27,14 @@ const Orders = () => {
                     width={"100%"}
                     height={"150px"}
                     element={
-                        <div>
-                            <div></div>
-                            <div>Orders Confirmed</div>
-                            <div>4.5</div>
+                        <div className={cssStyles.OrderElementBlock}>
+                            <div>{orderIcons[1].icon}</div>
+                            <div className={cssStyles.OrderElementTitle}>
+                                Orders Confirmed
+                            </div>
+                            <div className={cssStyles.OrderElementText}>
+                                323
+                            </div>
                         </div>
                     }
                 />
@@ -35,10 +42,12 @@ const Orders = () => {
                     width={"100%"}
                     height={"150px"}
                     element={
-                        <div>
-                            <div></div>
-                            <div>Orders Canceled</div>
-                            <div>4.5</div>
+                        <div className={cssStyles.OrderElementBlock}>
+                            <div>{orderIcons[2].icon}</div>
+                            <div className={cssStyles.OrderElementTitle}>
+                                Orders Canceled
+                            </div>
+                            <div className={cssStyles.OrderElementText}>17</div>
                         </div>
                     }
                 />
@@ -46,16 +55,139 @@ const Orders = () => {
                     width={"100%"}
                     height={"150px"}
                     element={
-                        <div>
-                            <div></div>
-                            <div>Orders Refunded </div>
-                            <div>4.5</div>
+                        <div className={cssStyles.OrderElementBlock}>
+                            <div>{orderIcons[3].icon}</div>
+                            <div className={cssStyles.OrderElementTitle}>
+                                Orders Refunded{" "}
+                            </div>
+                            <div className={cssStyles.OrderElementText}>2</div>
+                        </div>
+                    }
+                />
+            </div>
+            <div className={cssStyles.SecondBlock}>
+                <Cards
+                    width={"100%"}
+                    height={"800px"}
+                    element={
+                        <div className={cssStyles.gridtable}>
+                            <div className={cssStyles.gridtableBlock}>
+                                <div className={cssStyles.header}>Order</div>
+                                <div className={cssStyles.header}>Product</div>
+                                <div className={cssStyles.header}>SKU</div>
+                                <div className={cssStyles.header}>Category</div>
+                                <div className={cssStyles.header}>Payment</div>
+                                <div className={cssStyles.header}>
+                                    Order Status
+                                </div>
+                                <div className={cssStyles.header}>Rate</div>
+                                <div className={cssStyles.header}>Actions</div>
+                            </div>
+
+                            <div className={cssStyles.orderList}>
+                                <div>#12345</div>
+                                <div>
+                                    <img
+                                        style={{
+                                            width: "70px",
+                                            height: "70px",
+                                        }}
+                                        src={
+                                            "https://shop-point.merku.love/assets/1-a5b55edd.webp"
+                                        }
+                                        alt="s"
+                                    />
+                                </div>
+                                <div>123-ABC</div>
+                                <div>Electronics</div>
+                                <div>Credit Card</div>
+                                <div>Processing</div>
+                                <div>4.5</div>
+                                <div className="actions">
+                                    <a href="#">View</a> | <a href="#">Edit</a>{" "}
+                                    | <a href="#">Delete</a>
+                                </div>
+                            </div>
+
+                            <div className={cssStyles.orderList}>
+                                <div>#12346</div>
+                                <div>
+                                    <img
+                                        style={{
+                                            width: "70px",
+                                            height: "70px",
+                                        }}
+                                        src={
+                                            "https://shop-point.merku.love/assets/1-a5b55edd.webp"
+                                        }
+                                        alt="s"
+                                    />
+                                </div>
+                                <div>124-BCD</div>
+                                <div>Home Appliance</div>
+                                <div>PayPal</div>
+                                <div>Shipped</div>
+                                <div>5.0</div>
+                                <div className="actions">
+                                    <a href="#">View</a> | <a href="#">Edit</a>{" "}
+                                    | <a href="#">Delete</a>
+                                </div>
+                            </div>
+                            <div className={cssStyles.orderList}>
+                                <div>#12345</div>
+                                <div>
+                                    <img
+                                        style={{
+                                            width: "70px",
+                                            height: "70px",
+                                        }}
+                                        src={
+                                            "https://shop-point.merku.love/assets/1-a5b55edd.webp"
+                                        }
+                                        alt="s"
+                                    />
+                                </div>
+                                <div>123-ABC</div>
+                                <div>Electronics</div>
+                                <div>Credit Card</div>
+                                <div>Processing</div>
+                                <div>4.5</div>
+                                <div className="actions">
+                                    <a href="#">View</a> | <a href="#">Edit</a>{" "}
+                                    | <a href="#">Delete</a>
+                                </div>
+                            </div>
+
+                            <div className={cssStyles.orderList}>
+                                <div>#12346</div>
+                                <div>
+                                    <img
+                                        style={{
+                                            width: "70px",
+                                            height: "70px",
+                                        }}
+                                        src={
+                                            "https://shop-point.merku.love/assets/1-a5b55edd.webp"
+                                        }
+                                        alt="s"
+                                    />
+                                </div>
+                                <div>124-BCD</div>
+                                <div>Home Appliance</div>
+                                <div>PayPal</div>
+                                <div>Shipped</div>
+                                <div>5.0</div>
+                                <div className="actions">
+                                    <a href="#">View</a> | <a href="#">Edit</a>{" "}
+                                    | <a href="#">Delete</a>
+                                </div>
+                            </div>
                         </div>
                     }
                 />
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Orders
+export default Orders;
